a02caa32ac63ae28c35d75c971b6682cb6f39e70
Philip Brohan <philip@brohan.org>
2017-05-18 16:53:25 +0100

    First attempt at data rescue poster.

---
+++ data.rescue/data.rescue.R
@@ -0,0 +1,287 @@
+#!/usr/bin/Rscript --no-save
+
+# Poster comparing ERA5 with ERA Interim
+# Print quality - A0 format
+
+library(GSDF.TWCR)
+library(GSDF.WeatherMap)
+library(grid)
+library(lubridate)
+library(jpeg)
+
+Imagedir<-sprintf(".",Sys.getenv('SCRATCH'))
+
+Options<-WeatherMap.set.option(NULL)
+Options<-WeatherMap.set.option(Options,'land.colour',rgb(100,100,100,255,
+                                                       maxColorValue=255))
+Options<-WeatherMap.set.option(Options,'sea.colour',rgb(200,200,200,255,
+                                                       maxColorValue=255))
+Options<-WeatherMap.set.option(Options,'ice.colour',rgb(250,250,250,255,
+                                                       maxColorValue=255))
+Options<-WeatherMap.set.option(Options,'pole.lon',160)
+Options<-WeatherMap.set.option(Options,'pole.lat',45)
+
+Options<-WeatherMap.set.option(Options,'lat.min',-90)
+Options<-WeatherMap.set.option(Options,'lat.max',90)
+Options<-WeatherMap.set.option(Options,'lon.min',-190+50)
+Options<-WeatherMap.set.option(Options,'lon.max',190+50)
+Options$vp.lon.min<- -180+50
+Options$vp.lon.max<-  180+50
+Options<-WeatherMap.set.option(Options,'wrap.spherical',F)
+Options$precip.colour=c(0,0.2,0)
+Options$label.xp=0.995
+Options<-WeatherMap.set.option(Options,'obs.size',1.0)
+Options<-WeatherMap.set.option(Options,'obs.colour',rgb(255,215,0,255,
+                                                       maxColorValue=255))
+
+Options$ice.points<-1000000
+
+Options$mslp.base=101325                    # Base value for anomalies
+Options$mslp.range=50000                    # Anomaly for max contour
+Options$mslp.step=500                       # Smaller -> more contours
+Options$mslp.tpscale=5                      # Smaller -> contours less transparent
+Options$mslp.lwd=1
+Options$precip.colour=c(0,0.2,0)
+# Overrides mslp options
+contour.levels<-seq(-300,300,30)
+contour.levels<-abs(contour.levels)**1.5*sign(contour.levels)
+contour.levels<-contour.levels+Options$mslp.base
+
+# Load the 0.25 degree orography
+orog<-GSDF.ncdf.load(sprintf("%s/orography/elev.0.25-deg.nc",Sys.getenv('SCRATCH')),'data',
+                             lat.range=c(-90,90),lon.range=c(-180,360))
+orog$data[orog$data<0]<-0 # sea-surface, not sea-bottom
+is.na(orog$data[orog$data==0])<-TRUE
+
+get.member.at.hour<-function(variable,year,month,day,hour,member,version='3.5.1') {
+
+       t<-TWCR.get.members.slice.at.hour(variable,year,month,day,
+                                  hour,version=version)
+       t<-GSDF.select.from.1d(t,'ensemble',member)
+       gc()
+       return(t)
+  }
+
+# Plot the orography - raster background, fast
+draw.land.flat<-function(Options,n.levels=20) {
+   land<-GSDF.WeatherMap:::WeatherMap.rotate.pole(GSDF:::GSDF.pad.longitude(orog),Options)
+   lons<-land$dimensions[[GSDF.find.dimension(land,'lon')]]$values
+   base.colour<-Options$land.colour
+   plot.colours<-rep(rgb(0,0,0,0),length(land$data))
+      w<-which(land$data>0)
+      plot.colours[w]<-base.colour
+      m<-matrix(plot.colours, ncol=length(lons), byrow=TRUE)
+      r.w<-max(lons)-min(lons)+(lons[2]-lons[1])
+      r.c<-(max(lons)+min(lons))/2
+      grid.raster(m,,
+                   x=unit(r.c,'native'),
+                   y=unit(0,'native'),
+                   width=unit(r.w,'native'),
+                   height=unit(180,'native'))  
+}
+
+draw.temperature<-function(temperature,Options,Trange=1) {
+
+  Options.local<-Options
+  Options.local$fog.min.transparency<-0.5
+  tplus<-temperature
+  tplus$data[]<-pmax(0,pmin(Trange,tplus$data))/Trange
+  Options.local$fog.colour<-c(1,0,0)
+  WeatherMap.draw.fog(tplus,Options.local)
+  tminus<-temperature
+  tminus$data[]<-tminus$data*-1
+  tminus$data[]<-pmax(0,pmin(Trange,tminus$data))/Trange
+  Options.local$fog.colour<-c(0,0,1)
+  WeatherMap.draw.fog(tminus,Options.local)
+}
+
+
+draw.pressure<-function(mslp,Options,colour=c(0,0,0)) {
+
+  M<-GSDF.WeatherMap:::WeatherMap.rotate.pole(mslp,Options)
+  M<-GSDF:::GSDF.pad.longitude(M) # Extras for periodic boundary conditions
+  lats<-M$dimensions[[GSDF.find.dimension(M,'lat')]]$values
+  longs<-M$dimensions[[GSDF.find.dimension(M,'lon')]]$values
+    # Need particular data format for contourLines
+  maxl<-Options$vp.lon.max+2
+  if(lats[2]<lats[1] || longs[2]<longs[1] || max(longs) > maxl ) {
+    if(lats[2]<lats[1]) lats<-rev(lats)
+    if(longs[2]<longs[1]) longs<-rev(longs)
+    longs[longs>maxl]<-longs[longs>maxl]-(maxl*2)
+    longs<-sort(longs)
+    M2<-M
+    M2$dimensions[[GSDF.find.dimension(M,'lat')]]$values<-lats
+    M2$dimensions[[GSDF.find.dimension(M,'lon')]]$values<-longs
+    M<-GSDF.regrid.2d(M,M2)
+  }
+  z<-matrix(data=M$data,nrow=length(longs),ncol=length(lats))
+  lines<-contourLines(longs,lats,z,
+                       levels=contour.levels)
+  if(!is.na(lines) && length(lines)>0) {
+     for(i in seq(1,length(lines))) {
+         tp<-min(1,(abs(lines[[i]]$level-Options$mslp.base)/
+                    Options$mslp.tpscale))
+         lt<-5
+         lwd<-1
+         if(lines[[i]]$level<=Options$mslp.base) {
+             lt<-1
+             lwd<-1
+         }
+         gp<-gpar(col=rgb(colour[1],colour[2],colour[3],tp),
+                             lwd=Options$mslp.lwd*lwd,lty=lt)
+         res<-tryCatch({
+            grid.lines(x=unit(lines[[i]]$x,'native'),
+			y=unit(lines[[i]]$y,'native'),
+			gp=gp)
+             }, warning = function(w) {
+                 print(w)
+             }, error = function(e) {
+                print(e)
+             }, finally = {
+                # Do nothing
+             })
+     }
+  }
+}
+
+
+# Intensity function for precipitation
+set.precip.value<-function(rate) {
+  min.threshold<-0.0025
+  max.threshold<-0.03
+  rate<-sqrt(rate)
+  result<-rep(NA,length(rate))
+  value<-pmax(0,pmin(1,rate/max.threshold))
+  w<-which(runif(length(rate),0,1)<value & rate>min.threshold)
+  if(length(w)>0) result[w]<-value[w]
+  return(result)
+}
+
+# Colour function for t2m
+set.t2m.colour<-function(temperature,Trange=3) {
+
+  result<-rep(NA,length(temperature))
+  w<-which(temperature>0)
+  if(length(w)>0) {
+     temperature[w]<-sqrt(temperature[w])
+     temperature[w]<-pmax(0,pmin(Trange,temperature[w]))
+     temperature[w]<-temperature[w]/Trange
+     temperature[w]<-round(temperature[w],1)
+     result[w]<-rgb(1,0,0,temperature[w]*0.6)
+  }
+  w<-which(temperature<0)
+  if(length(w)>0) {
+     temperature[w]<-sqrt(temperature[w]*-1)
+     temperature[w]<-pmax(0,pmin(Trange,temperature[w]))
+     temperature[w]<-temperature[w]/Trange
+     temperature[w]<-round(temperature[w],1)
+     result[w]<-rgb(0,0,1,temperature[w]*0.6)
+ }
+ return(result)
+}
+
+
+# Colour function for streamlines
+set.streamline.GC<-function(Options) {
+
+   alpha<-255
+   return(gpar(col=rgb(125,125,125,alpha,maxColorValue=255),
+               fill=rgb(125,125,125,alpha,maxColorValue=255),lwd=1.5))
+}
+
+# Rotate the pole
+set.pole<-function(step,Options) {
+  lon<-160+(step*20)
+  if(lon>360) lon<-lon%%360
+  lat<-35+sin(step)*20
+  Options<-WeatherMap.set.option(Options,'pole.lon',lon)
+  Options<-WeatherMap.set.option(Options,'pole.lat',lat)
+  min.lon<-(step*20)%%360-180
+  Options<-WeatherMap.set.option(Options,'lon.min',min.lon-10)
+  Options<-WeatherMap.set.option(Options,'lon.max',min.lon+380)
+  Options<-WeatherMap.set.option(Options,'vp.lon.min',min.lon   )
+  Options<-WeatherMap.set.option(Options,'vp.lon.max',min.lon+360)
+  return(Options)
+}
+  
+# Make a sub plot
+sub.plot<-function(year,month,day,hour,Options) {
+  
+  lon.min<-Options$lon.min
+  if(!is.null(Options$vp.lon.min)) lon.min<-Options$vp.lon.min
+  lon.max<-Options$lon.max
+  if(!is.null(Options$vp.lon.max)) lon.max<-Options$vp.lon.max
+  lat.min<-Options$lat.min
+  if(!is.null(Options$vp.lat.min)) lat.min<-Options$vp.lat.min
+  lat.max<-Options$lat.max
+  if(!is.null(Options$vp.lat.max)) lat.max<-Options$vp.lat.max
+
+  pushViewport(dataViewport(c(lon.min,lon.max),c(lat.min,lat.max),
+                            extension=0,gp=base.gp))
+
+  icec<-TWCR.get.slice.at.hour('icec',year,month,day,hour,version='3.5.1')
+  ip<-WeatherMap.rectpoints(Options$ice.points,Options)
+  WeatherMap.draw.ice(ip$lat,ip$lon,icec,Options)
+  draw.land.flat(Options)
+  
+  t2m<-get.member.at.hour('air.2m',year,month,day,hour,1,version='3.5.1')
+  t2n<-TWCR.get.slice.at.hour('air.2m',year,month,day,hour,type='normal',version='3.4.1')
+  t2n<-GSDF.regrid.2d(t2n,t2m)
+  t2m$data[]<-t2m$data-t2n$data
+  draw.temperature(t2m,Options,Trange=10)
+  
+  prmsl<-get.member.at.hour('prmsl',year,month,day,hour,1,version='3.5.1')
+  draw.pressure(prmsl,Options)
+  
+  prate<-get.member.at.hour('prate',year,month,day,hour,1,version='3.5.1')
+  WeatherMap.draw.precipitation(prate,Options)
+  
+  obs<-TWCR.get.obs(year,month,day,hour,version='3.5.1')
+  WeatherMap.draw.obs(obs,Options)
+
+  Options$label=sprintf("%04d-%02d-%02d:%02d",year,month,day,as.integer(hour))
+    Options<-WeatherMap.set.option(Options,'land.colour',Options$sea.colour)
+    WeatherMap.draw.label(Options)
+
+  popViewport()
+}
+
+# Make the full plot
+
+image.name<-sprintf("data.rescue.pdf",year,month,day,hour)
+ifile.name<-sprintf("%s/%s",Imagedir,image.name)
+
+ pdf(ifile.name,
+         width=46.8,
+         height=33.1,
+         bg=rgb(220,220,220,255,maxColorValue=255),
+         family='Helvetica',
+         pointsize=12)
+
+  base.gp<-gpar(fontfamily='Helvetica',fontface='bold',col='black')
+
+ base.date<-lubridate::ymd_hms("1860-01-22:06:00:00")
+ count<-0
+ for(j in seq(1,5)) {
+    for(i in seq(1,4)) {
+       date<-base.date+years(8*count)+hours(400*count)
+       #Options<-set.pole(count,Options)
+       pushViewport(viewport(x=unit((i-0.5)/4,'npc'),
+                             y=unit((5.5-j)/5,'npc'),
+                             width=unit((1/5)*1.1,'npc'),
+                             height=unit((1/5)/sqrt(2)*1.1,'npc'),
+                             clip='on'))
+          grid.polygon(x=unit(c(0,1,1,0),'npc'),
+                       y=unit(c(0,0,1,1),'npc'),
+                       gp=gpar(col=Options$sea.colour,fill=Options$sea.colour))
+          sub.plot(lubridate::year(date),
+                   lubridate::month(date),
+                   lubridate::day(date),
+                   lubridate::hour(date),
+                   Options)
+       popViewport()
+       count<-count+1
+     }
+  }
+  dev.off()
+
