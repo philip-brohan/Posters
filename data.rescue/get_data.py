68bf66cd4d5bb3ea860358bd3aeeed25868a2cfe
Philip Brohan <philip@brohan.org>
2017-09-18 17:16:25 +0100

    Improvements to script to fetch data - better handling of data when it's not all available.

--- data.rescue/get_data.py
+++ data.rescue/get_data.py
@@ -4,22 +4,18 @@ import twcr
 import datetime
 
 def retrieve_for_year(year,version,source=None):
+   twcr.fetch_data_for_year('prmsl',year,version,
+                            type='ensemble',source='released')
+   twcr.fetch_data_for_year('air.2m',year,version,
+                            type='ensemble',source='released')
+   twcr.fetch_data_for_year('prate',year,version,
+                            type='ensemble',source='released')
+   twcr.fetch_data_for_year('prmsl',year,version,
+                            type='first.guess.mean',source='scratch')
+   twcr.fetch_data_for_year('prmsl',year,version,
+                            type='first.guess.spread',source='scratch')
    twcr.fetch_data_for_year('observations',year,
                             version,source=source)
-   twcr.fetch_data_for_year('prmsl',year,version,
-                            type='mean',source=source)
-   twcr.fetch_data_for_year('prmsl',year,version,
-                            type='spread',source=source)
-   twcr.fetch_data_for_year('prmsl',year,version,
-                            type='first.guess.mean',source=source)
-   twcr.fetch_data_for_year('prmsl',year,version,
-                            type='first.guess.spread',source=source)
-   twcr.fetch_data_for_year('prmsl',year,version,
-                            type='ensemble',source=source)
-   twcr.fetch_data_for_year('air.2m',year,version,
-                            type='ensemble',source=source)
-   twcr.fetch_data_for_year('prate',year,version,
-                            type='ensemble',source=source)
 
 
 base_date=datetime.datetime.strptime("1872-12-02:12:00:00",
@@ -30,6 +26,8 @@ for i in range(1,5):
     for j in range(1,6):
         t_date=base_date+date_step*count
         print(t_date.year)
-        #retrieve_for_year(t_date.year,'3.2.1')
-        #retrieve_for_year(t_date.year,'3.5.1')
+        try:
+           retrieve_for_year(t_date.year,'3.2.1',source='scratch')
+        except:
+           print "Failed 3.2.1 retrieval for %d" % t_date.year
         count=count+1
