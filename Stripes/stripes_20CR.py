a1573c5eb8f9ce8c212c5547ea0921256dbf7037
Philip Brohan <philip@brohan.org>
2019-12-19 17:28:06 +0000

    Experimentation with 20CR warming stripes.

--- Stripes/stripes_20CR.py
+++ Stripes/stripes_20CR.py
@@ -5,7 +5,7 @@
 
 import os
 import iris
-import geohash2
+#import geohash2
 import numpy
 import datetime
 
@@ -17,16 +17,18 @@ from matplotlib.patches import Rectangle
 from pandas import qcut
 
 # Load the 20CR data
-h=iris.load_cube('%s/Posters/HadCRUT/air.2m.mon.mean.nc' %
-            os.getenv('SCRATCH'),'air_temperature')
+h=iris.load_cube('./air.2m.mon.mean.nc','air_temperature')
 # Get the climatology
-n=iris.load_cube('%s/Posters/HadCRUT/air.2m.mon.ltm.nc' %
-            os.getenv('SCRATCH'),'air_temperature')
+n=[]
+for m in range(1,13):
+    mc=iris.Constraint(time=lambda cell: cell.point.month == m and cell.point.year>1700 and cell.point.year<2050)
+    n.append(h.extract(mc).collapsed('time', iris.analysis.MEAN))
+
 # Anomalise
 for tidx in range(len(h.coord('time').points)):
     tpt=datetime.datetime(1800,1,1)+datetime.timedelta(hours=h.coord('time').points[tidx])
     midx=tpt.month-1
-    h.data[tidx,:,:] -= n.data[midx,:,:]
+    h.data[tidx,:,:] -= n[midx].data
 
 # Average over longitude
 h=h.collapsed('longitude', iris.analysis.MEAN)
@@ -45,7 +47,7 @@ ndata=h.data
 #    ndata[t,:]=[x for _,x in sorted(zip(ghl,ndata[t,:]))]
 
 # Plot the resulting array as a 2d colourmap
-fig=Figure(figsize=(60,35),              # Width, Height (inches)
+fig=Figure(figsize=(8,2),              # Width, Height (inches)
            dpi=300,
            facecolor=(0.5,0.5,0.5,1),
            edgecolor=None,
@@ -62,13 +64,19 @@ ax.set_axis_off() # Don't want surrounding x and y axis
 # Axes ignores facecolor, so set background explicitly
 #ax.add_patch(Rectangle((0,0),1,1,facecolor=(0.88,0.88,0.88,1),fill=True,zorder=1))
 
-ndata=numpy.transpose(ndata)
+# Nomalise by latitude
 s=ndata.shape
-ndata=qcut(ndata.flatten(),200,labels=False,
-                             duplicates='drop').reshape(s),
+for lat in range(s[1]):
+   qn=qcut(ndata[:,lat],100,labels=False,duplicates='drop')
+   ndata[:,lat]=qn
+
+ndata=numpy.transpose(ndata)
+#s=ndata.shape
+#ndata=qcut(ndata.flatten(),200,labels=False,
+#                             duplicates='drop').reshape(s),
 y = numpy.linspace(0.001,0.999,s[0])
 x = numpy.linspace(0.001,0.999,s[1])
-img = ax.pcolorfast(x,y,ndata[0],
+img = ax.pcolorfast(x,y,ndata,
                         cmap='RdYlBu_r',
                         alpha=1.0,
                         zorder=100)
